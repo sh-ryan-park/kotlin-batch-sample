6ed41a7d71a206fff2b4cec46c88d15936d147d0
Ryan(Sanghyun Park) <sh.ryan.park@dreamus.io>
2024-02-23 03:34:19 +0900

    kotlin batch sample 2

--- src/main/kotlin/com/example/kotlinbatch/sample/SampleJob.kt
+++ src/main/kotlin/com/example/kotlinbatch/sample/SampleJob.kt
@@ -1,8 +1,13 @@
 package com.example.kotlinbatch.sample
 
-import com.example.kotlinbatch.sample.repo.PurchasePassEntitySupport
+import com.example.kotlinbatch.sample.repo.PurchasePassEntity
+import com.example.kotlinbatch.sample.repo.PurchasePassEntitySupport.memberNo
+import com.example.kotlinbatch.sample.repo.PurchasePassEntitySupport.prchsId
+import com.example.kotlinbatch.sample.repo.PurchasePassEntitySupport.purchasePass
 import com.example.kotlinbatch.sample.repo.PurchasePassMapper
 import com.example.kotlinbatch.sample.repo.select
+import org.mybatis.dynamic.sql.util.kotlin.spring.select
+import org.mybatis.dynamic.sql.util.kotlin.spring.selectOne
 import org.springframework.batch.core.*
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
@@ -12,6 +17,7 @@ import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.transaction.PlatformTransactionManager
 
 /**
@@ -21,11 +27,12 @@ import org.springframework.transaction.PlatformTransactionManager
 class SampleJob(
     private val jobRepository: JobRepository,
     private val transactionManager: PlatformTransactionManager,
-    private val purchasePassMapper: PurchasePassMapper
+    private val purchasePassMapper: PurchasePassMapper,
+    private val jdbcTemplate: NamedParameterJdbcTemplate,
 ) {
 
     @Bean
-    fun job() : Job {
+    fun job(): Job {
         return JobBuilder("sample", jobRepository)
             .start(step())
             .listener(object : JobExecutionListener {
@@ -50,16 +57,26 @@ class SampleJob(
     @Bean
     fun tasklet(): Tasklet {
         return Tasklet { contribution, chunkContext ->
-            println("job run")
+            println("tasklet started")
 
             val rows = purchasePassMapper.select {
                 where {
-                    PurchasePassEntitySupport.memberNo isEqualTo 621
+                    memberNo isEqualTo 621
                 }
             }
 
-            rows.forEach { println(it) }
+            rows.forEach { println("mybatis mapper result $it") }
 
+            val provider = select(prchsId, memberNo) {
+                from(purchasePass)
+                where { memberNo isEqualTo 621 }
+                orderBy(prchsId)
+            }
+
+            jdbcTemplate.selectOne(provider) { rs, _ -> PurchasePassEntity(rs.getLong(1), rs.getLong(2)) }
+                .also { println("jdbc template result $it") }
+
+            println("tasklet end")
             RepeatStatus.FINISHED
         }
     }
